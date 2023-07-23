01d88e687519ca8d1d5608aa897bea51a7f32266
CamilleForini <camillebatista54@gmail.com>
2023-07-23 18:05:00 -0300

    Added song list test and initiated rank valid test

--- add_songs_db.py
+++ add_songs_db.py
@@ -3,12 +3,12 @@ from mysql_connection import connection
 
 
 # Opens txt files with song list
-with open("ts_songs.txt", "r") as ts_songs_file:
-    ts_song_list = ts_songs_file.readlines()
+with open("teste_list.txt", "r") as songs_file:
+    song_list = songs_file.readlines()
 
-ts_song_list_formatted = []
-for song in ts_song_list:
-    ts_song_list_formatted.append(song.strip().title())
+song_list_formatted = []
+for song in song_list:
+    song_list_formatted.append(song.strip().title())
 
 # MYSQL CONNECTION
 myconnection = connection()
@@ -16,7 +16,7 @@ myconnection = connection()
 # ADDS SONG NAME TO DB
 with myconnection:
     with myconnection.cursor() as cursor:
-        for song in ts_song_list_formatted:
+        for song in song_list_formatted:
             sql = f"INSERT INTO ts_songs (name) VALUE ('{song}')"
             cursor.execute(sql)
             myconnection.commit()
--- main.py
+++ main.py
@@ -3,18 +3,26 @@ from mysql_connection import connection
 myconnection = connection()
 TABLE_NAME = "ts_songs"
 
-song_list = []
-with myconnection:
+# FETCH LIST OF SONGS IN DATABASE
+song_list = ["False god", "Cornelia Street", "Daylight"]
+"""with myconnection:
     with myconnection.cursor() as cursor:
         cursor.execute(f"SELECT * FROM {TABLE_NAME}")
-
         for row in cursor.fetchall():
             name, album, rank = row
-            song_list.append(name)
+            song_list.append(name)"""
 
-        for song in song_list:
-            new_rank = int(input(f"Digite o rank para a musica {song} "))
-            cursor.execute(
-                f"UPDATE {TABLE_NAME} SET song_rank='{new_rank}' WHERE name='{song}'"
-            )
-            myconnection.commit()
+ranks_used = []
+for song in song_list:
+    rank = input(f"Enter the ranking of this song {song}: ")
+    rank = int(rank)
+
+    # checks if song rankin is in range
+    if rank > len(song_list):
+        print(f"Rank out of range, please rank song from 1 to {len(song_list)}")
+        rank = int(input(f"Enter the ranking of this song {song}: "))
+    if rank in ranks_used:  # checks if rank already used
+        print("Rank already used")
+        rank = int(input(f"Enter the ranking of this song {song}: "))
+    else:
+        ranks_used.append(rank)
