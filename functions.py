c6a26c1cf03dae309cb5c7e82e2d2f0f47637b4f
CamilleForini <camillebatista54@gmail.com>
2023-07-26 17:32:01 -0300

    edited checkrank function

--- functions.py
+++ functions.py
@@ -12,16 +12,28 @@ def openfile(filename):
 
 
 def checkrank(rank, song, song_list, ranks_used):
-    """Checks the rank of a given song"""
+    """Checks the rank of a given song and returns a valid rank"""
+    rank = rank
     while True:
-        if rank > len(song_list):
-            print(f"Rank out of range, expecting value between 1 and {len(song_list)}")
-            rank = int(input(f"Please enter a rank to rank this song {song} "))
+        if rank.isdigit():
+            rank_int = int(rank)
+        else:
+            print("Invalid rank, please enter only numbers")
+            rank = input(f"Please enter a rank to rank this song {song} ")
             continue
-        elif rank in ranks_used:
+
+        if rank_int > len(song_list):
+            print(f"Rank out of range, expecting value between 1 and {len(song_list)}")
+            rank = input(f"Please enter a rank to rank this song {song} ")
+            continue
+        elif rank_int in ranks_used:
             print("Rank already used")
-            rank = int(input(f"Please enter a rank to rank this song {song} "))
+            rank = input(f"Please enter a rank to rank this song {song} ")
             continue
         else:
             break
-    return True
+    return rank_int
+
+
+def rank_int(rank):
+    """Validates"""
--- main.py
+++ main.py
@@ -12,10 +12,10 @@ for row in cursor.fetchall():
 songs_ranked = {}
 ranks_used = []
 for song in song_list:
-    rank = int(input(f"Enter the rank of the song {song} "))
-    if checkrank(rank, song, song_list, ranks_used):
-        songs_ranked[rank] = song
-        ranks_used.append(rank)
+    rank = input(f"Enter the rank of the song {song} ")
+    rank_checked = checkrank(rank, song, song_list, ranks_used)
+    songs_ranked[rank_checked] = song
+    ranks_used.append(rank_checked)
 
 for rank, song in songs_ranked.items():
     print(rank, "-", song)
